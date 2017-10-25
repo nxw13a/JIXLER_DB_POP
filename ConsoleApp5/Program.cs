a89c22ea9076b02bed819c96b89eebfcd5166e75
nxw13a <nxw13a@acu.edu>
2017-10-25 17:41:08 -0500

    Update Program.cs

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -18,6 +18,7 @@ using MongoDB.Driver;
 using MongoDB.Bson;
 namespace ConsoleApp5
 {
+    //Database Data
     public class Location_
     {
         public string _id { get; set; }
@@ -28,6 +29,14 @@ namespace ConsoleApp5
         public string Location { get; set; }
         public string START_DATE { get; set; }
         public double[] currentLocation { get; set; }
+
+        public string END_DATE { get; set; }
+        public string _p_FromUser { get; set; }
+        public string accessType { get; set; }
+        public string jixCategory { get; set; }
+        public string jixRadius { get; set; }
+        public string _p_userid { get; set; }
+        //public string jixMessage { get; set; }
     }
 
     class Program
@@ -45,9 +54,20 @@ namespace ConsoleApp5
 
             return (degrees + (minutes / 60) + (seconds / 3600))*number;
         }
+        private static String error_ad = "\r\nPaid Advertisement\r\nClick on the ad for more information.\r\nPlease report objectionable advertising to the Editor.";
+        public static String remove_ad(String sentences)
+        {
+            int index = sentences.IndexOf(error_ad);
+            if(index != - 1)
+            {
+                return sentences.Remove(index);
+            }
+            return sentences;
+        }
 
         static void Main(string[] args)
         {
+            
             string connectionString = "mongodb://52.33.219.239:27017";
             MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
             MongoClient client = new MongoClient(settings);
@@ -56,135 +76,163 @@ namespace ConsoleApp5
 #pragma warning restore CS0618 // Type or member is obsolete
             MongoDatabase db = server.GetDatabase("dev");
             MongoCollection<Location_> collection = db.GetCollection<Location_>("JixInfo");
+            
 
             IWebDriver driver = new ChromeDriver(@"C:\Users\natta\Downloads\");
-            driver.Navigate().GoToUrl("https://www.hmdb.org//");
-            driver.FindElement(By.XPath("//*[@title='Markers near your present location']")).Click();
+            driver.Navigate().GoToUrl("https://www.hmdb.org/search.asp");
+  
+            driver.FindElement(By.XPath("//*[@name='Zip']")).SendKeys("79605");
+            driver.FindElement(By.XPath("//*[@name='Zip']")).SendKeys(Keys.Enter);
             Thread.Sleep(2000);
-            driver.FindElement(By.XPath("//*[@value='List of Markers Near Your Location']")).Click();
-            driver.FindElement(By.PartialLinkText("Company I   7th Texas Infantry")).Click();
+            
+            IList<IWebElement> all = driver.FindElements(By.XPath("//*/article/table/tbody/tr/td/a"));
 
-            Thread.Sleep(2000);
-            String name = driver.FindElement(By.XPath("//h1")).Text.ToString();
-
-            String hold_long = driver.FindElement(By.XPath("(//*[@class='bodyserif'])")).Text.ToString();
-            int begin = hold_long.IndexOf("Location.");
-            int holder = hold_long.IndexOf("Erected");
-            int begin1 = hold_long.IndexOf("Inscription. ");
-
-            String longi = hold_long.Substring(begin + 10,13);
-            String lat = hold_long.Substring(begin + 10 + 15, 13);
-            String info = hold_long.Substring(begin1 + 13, holder - begin1 - 13);
-
-            double degree = Convert.ToDouble(longi.Substring(0, 2));
-            double minutes = Convert.ToDouble(longi.Substring(4, 6));
-            double total = ConvertDegreeAngleToDouble(degree,minutes,0,longi.Substring(longi.Length-1,1));
-            longi = System.Convert.ToString(total);
-            double[] hold_num1 = new double[2];
-            hold_num1[1] = total;
-            double degree1 = Convert.ToDouble(lat.Substring(0, 2));
-            double minutes1 = Convert.ToDouble(lat.Substring(4, 6));
-            double total1 = ConvertDegreeAngleToDouble(degree1, minutes1, 0, lat.Substring(lat.Length - 1, 1));
-            lat = System.Convert.ToString(total1);
-            hold_num1[0] = total1;
-            Console.WriteLine(name);
-            Console.WriteLine(longi);
-            Console.WriteLine(lat);
-            Console.WriteLine(info);
-
-            driver.Navigate().Back();
-            Thread.Sleep(2000);
-            driver.FindElement(By.PartialLinkText("Abilene Woman's Club Building")).Click();
-
-            String name1 = driver.FindElement(By.XPath("//h1")).Text.ToString();
-            String hold_long1 = driver.FindElement(By.XPath("(//*[@class='bodyserif'])")).Text.ToString();
-            int begin2 = hold_long1.IndexOf("Location.");
-            int holder1 = hold_long1.IndexOf("Erected");
-            int begin11 = hold_long1.IndexOf("Inscription. ");
-
-            String longi1 = hold_long1.Substring(begin2 + 10, 13);
-            String lat1 = hold_long1.Substring(begin2 + 10 + 15, 13);
-            String info1 = hold_long1.Substring(begin11 + 13, holder1 - begin11 - 13);
-
-            double degree11 = Convert.ToDouble(longi1.Substring(0, 2));
-            double minutes11 = Convert.ToDouble(longi1.Substring(4, 6));
-            double total11 = ConvertDegreeAngleToDouble(degree11, minutes11, 0, longi1.Substring(longi1.Length - 1, 1));
-            longi1 = System.Convert.ToString(total11);
-            double[] hold_num = new double[2];
-            hold_num[1] = total11;
-
-            double degree12 = Convert.ToDouble(lat1.Substring(0, 2));
-            double minutes12 = Convert.ToDouble(lat1.Substring(4, 6));
-            double total12 = ConvertDegreeAngleToDouble(degree12, minutes12, 0, lat1.Substring(lat1.Length - 1, 1));
-            lat1 = System.Convert.ToString(total12);
-            hold_num[0] = total12;
-
-            Console.WriteLine(name1);
-            Console.WriteLine(longi1);
-            Console.WriteLine(lat1);
-            Console.WriteLine(info1);
-
-
-
-            Location_ a = new Location_
+            String[] allText = new String[all.Count];
+            int i = 0;
+            Console.Write("Number of Item(s): ");
+            Console.WriteLine(all.Count / 2);
+            Console.Write("Number of Wanted Item(s): ");
+            int require_num = Convert.ToInt32(Console.ReadLine()); ;
+            
+            foreach (IWebElement element in all)
             {
-                _id = ObjectId.GenerateNewId().ToString(),
-                jixName = name,
-                longitude = lat,
-                lattitude = longi,
-                JIX_TELL_ABOUT = info,
-                Location = lat + "feet," + longi + "feet",
-                START_DATE = "Sept 25, 2017 at 10:45 AM",
-                currentLocation = hold_num1
-            };
-            Location_ b = new Location_
+                    allText[i++] = element.Text;
+                    //Console.WriteLine(allText[i - 1]);
+            }
+            //Console.WriteLine(all.Count);
+            String[] certainText = new String[all.Count/2];
+            for (int y = 0; y < all.Count / 2; y++)
             {
-                _id = ObjectId.GenerateNewId().ToString(),
-                jixName = name1,
-                longitude = lat1,
-                lattitude = longi1,
-                JIX_TELL_ABOUT = info1,
-                Location = lat1 + "feet," + longi1+"feet",
-                START_DATE = "Sept 25, 2017 at 10:45 AM",
-                currentLocation = hold_num
-                
-           
-            };
+                certainText[y] = allText[2 * y + 1];
+                //Console.WriteLine(allText[2*y + 1]);               
+            }
+            for(int x = 0; x < require_num; x++)
+            {
+                //Console.WriteLine(certainText[x]);
+                driver.FindElement(By.PartialLinkText(certainText[x])).Click();
 
-            collection.Save(a);
-            collection.Save(b);
-            /*
-            driver.Navigate().GoToUrl("http://www.realtor.com/");
-            driver.FindElement(By.Id("searchBox")).SendKeys("10024");
-            driver.FindElement(By.XPath("//*[@class='btn btn-primary js-searchButton']")).Click();
-            Thread.Sleep(2000);
-            driver.FindElement(By.Id("srp-sortby")).Click();
-            driver.FindElement(By.Id("srp-sortby")).SendKeys(Keys.Down + Keys.Enter);
-            String price = driver.FindElement(By.XPath("//*[@class='data-price-display']")).Text.ToString();
-            String address = driver.FindElement(By.XPath("//*[@class='srp-item-address ellipsis']")).Text.ToString();
-            String price2 = driver.FindElement(By.XPath("(//*[@class='data-price-display'])[2]")).Text.ToString();
-            String address2 = driver.FindElement(By.XPath("(//*[@class='srp-item-address ellipsis'])[2]")).Text.ToString();
-            Console.WriteLine();
-            Console.WriteLine(price);
-            Console.WriteLine(address);
-            Console.WriteLine(price2);
-            Console.WriteLine(address2);
-            House a = new House
-            {
-                Id = ObjectId.GenerateNewId().ToString(),
-                Value = price,
-                Address = address
-            };
-            House b = new House
-            {
-                Id = ObjectId.GenerateNewId().ToString(),
-                Value = price2,
-                Address = address2
-            };
-            collection.Save(a);
-            collection.Save(b);
+                Thread.Sleep(2000);
+                String name = driver.FindElement(By.XPath("//h1")).Text.ToString();
 
-            */
+                String hold_long = driver.FindElement(By.XPath("(//*[@class='bodyserif'])")).Text.ToString();
+                int begin = hold_long.IndexOf("Location.");
+                int holder = hold_long.IndexOf("Erected");
+                int begin1 = hold_long.IndexOf("Inscription. ");
+                int number = begin + 10;
+                int last_n = number;
+                while(true)
+                {
+                    if (hold_long.ElementAt(number) == 'N' || hold_long.ElementAt(number) == 'E' || hold_long.ElementAt(number) == 'S' || hold_long.ElementAt(number) == 'W')
+                        break;
+                    number++;
+                }
+                //Console.WriteLine(number - last_n);
+                String longi = hold_long.Substring(begin + 10, number - last_n + 1);
+                number = begin + 10;
+                while (true)
+                {
+                    if (hold_long.ElementAt(number) == ',')
+                        break;
+                    number++;
+                }
+                //Console.WriteLine(number - last_n);
+                String lat = hold_long.Substring(begin + 10 + number - last_n + 2, 13);
+
+                String info = hold_long.Substring(begin1 + 13, holder - begin1 - 13);
+                info = remove_ad(info);
+                //Console.WriteLine(longi); Console.WriteLine(lat);
+                number = 0;
+                last_n = number;
+                while (true)
+                {
+                    if (longi.ElementAt(number) == '°')
+                        break;
+                    number++;
+                }
+                double degree = Convert.ToDouble(longi.Substring(0, number));
+                number = number + 2;
+                last_n = number;
+                while (true)
+                {
+                    //Console.WriteLine(lat.ElementAt(number));
+                    if (longi.ElementAt(number) == ' ')
+                        break;
+                    number++;
+                }
+                double minutes = Convert.ToDouble(longi.Substring(last_n, number - last_n - 1));
+                double total = ConvertDegreeAngleToDouble(degree, minutes, 0, longi.Substring(longi.Length - 1, 1));
+                longi = System.Convert.ToString(total);
+                double[] hold_num1 = new double[2];
+                hold_num1[1] = total;
+                number = 0;
+                last_n = number;
+                while (true)
+                {
+                    if (lat.ElementAt(number) == '°')
+                        break;
+                    number++;
+                }
+                //Console.WriteLine(number);
+                //Console.WriteLine(lat);
+                double degree1 = Convert.ToDouble(lat.Substring(0, number));
+                number = number + 2;
+                last_n = number;
+                while (true)
+                {
+                    //Console.WriteLine(lat.ElementAt(number));
+                    if (lat.ElementAt(number) == ' ')
+                        break;
+                    number++;
+                }
+                double minutes1 = Convert.ToDouble(lat.Substring(last_n, number - last_n - 1));
+                //Console.WriteLine(minutes); Console.WriteLine(minutes1);
+                double total1 = ConvertDegreeAngleToDouble(degree1, minutes1, 0, lat.Substring(lat.Length - 1, 1));
+                lat = System.Convert.ToString(total1);
+                hold_num1[0] = total1;
+
+                ///////////////////////just added
+                number = 0;
+                while(true)
+                {
+                    if (info.ElementAt(number) == '.')
+                        break;
+                    number++;
+                }
+                string message = info.Substring(0, number) + "...";
+                ///////////////////////just added
+
+                Console.WriteLine(name);
+                Console.WriteLine(longi);
+                Console.WriteLine(lat);
+                Console.WriteLine(info);
+
+                ///////////////////////just added
+                Console.WriteLine(message);
+
+                driver.Navigate().Back();
+                Thread.Sleep(2000);
+                Location_ a = new Location_
+                {
+                    _id = ObjectId.GenerateNewId().ToString(),
+                    jixName = name,
+                    longitude = lat,
+                    lattitude = longi,
+                    JIX_TELL_ABOUT = info,
+                    Location = lat + "feet," + longi + "feet",
+                    START_DATE = "Oct 20, 2017 at 1:50 PM",
+                    currentLocation = hold_num1,
+
+                    END_DATE = "Nil",
+                    _p_FromUser = "_User$IVkaHfwjrU",
+                    accessType = "Public",
+                    jixCategory = "Jixwiki",
+                    jixRadius = "10",
+                    _p_userid = "_User$IVkaHfwjrU",
+                    //jixMessage = message
+                };
+                collection.Save(a);
+            }
+   
             Console.ReadKey();
             driver.Close();
         }
